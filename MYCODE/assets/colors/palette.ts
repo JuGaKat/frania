3eea22ff4107cf072f0b9a4817eb8721a43d9b82
Justyna Gardener <justyna.gardener@o2.pl>
2023-05-11 10:26:24 +0200

    widok PrzyciskScreen bledy od momentu dodania tablicy i flatlisty

---
+++ MYCODE/assets/colors/palette.ts
@@ -0,0 +1,9 @@
+export const palette = {
+  colorOne: '#fff5ed',
+  colorTwo: '#FAE0D8',
+  colorThree: '#F0F4BF',
+  colorFour: '#DFE1BE',
+  colorFive: '#F1DEEE',
+  colorSix: '#C4B7BB',
+  colorSeven: '#FF9899',
+};
